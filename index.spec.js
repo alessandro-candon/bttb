7dd4168b1c0bae2259142e70b6e85f1b2f1e776a
Ale <alexcandy91@hotmail.it>
2023-02-10 15:31:45 +0100

    zero result

--- index.spec.js
+++ index.spec.js
@@ -4,7 +4,7 @@ describe('test max-profit', () => {
   it.each([
     [[3,3,5,0,0,3,1,4],6],
     [[1,2,3,4,5], 4],
-    [[7,6,4,3,1], 4],
+    [[7,6,4,3,1], 0],
   ])('with %p expecting %p', (prices, max) => {
 
     expect(maxProfit(prices)).toEqual(max);
